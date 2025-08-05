7df2eb73080fbd2058057f3b8f243adbb96e36ee
SherlocksHolmesc <144738101+SherlocksHolmesc@users.noreply.github.com>
2025-08-05 15:14:42 +0800

    small changes on the hardhat configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,27 @@
-import { HardhatUserConfig } from "hardhat/config";
 require("@nomicfoundation/hardhat-toolbox");
 require("@oasisprotocol/sapphire-hardhat");
 require("dotenv").config();
 
-const accounts = process.env.PRIVATE_KEY ? [`${process.env.PRIVATE_KEY}`] : [];
+const privateKey = process.env.PRIVATE_KEY || "";
 
-const config: HardhatUserConfig = {
+/** @type {import('hardhat/config').HardhatUserConfig} */
+module.exports = {
   solidity: "0.8.28",
   networks: {
     sapphireTestnet: {
       url: "https://testnet.sapphire.oasis.dev",
       chainId: 23295,
-      accounts: accounts,
+      accounts: [privateKey],
     },
     sapphireLocalnet: {
       url: "http://localhost:8545",
       chainId: 0x5afd,
-      accounts: accounts,
+      accounts: [privateKey],
     },
     arbitrumSepolia: {
       url: `https://arbitrum-sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       chainId: 421614,
-      accounts: accounts,
+      accounts: [privateKey],
     },
   },
 };
-
-export default config;
